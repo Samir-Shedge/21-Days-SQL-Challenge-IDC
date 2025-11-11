90d6beb1b020a0fd8ee60472cfe930d0ea129f2f
Samir-Shedge <samirshedge4153@gmail.com>
2025-11-11 15:33:22 +0530

    Day 8 submission

---
+++ Day08_String_Functions/practice_questions.sql
@@ -0,0 +1,8 @@
+-- 1. Convert all patient names to uppercase.
+SELECT UPPER(name) AS name FROM patients;
+
+-- 2. Find the length of each staff member's name.
+SELECT staff_name, LENGTH(staff_name) AS name_length FROM staff;
+
+-- 3. Concatenate staff_id and staff_name with a hyphen separator.
+SELECT CONCAT(staff_id, ' - ', staff_name) AS staff_info FROM staff;
